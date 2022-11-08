cd408ac9c09426379a5a59e205494ab7c55586d8
tnayem <tasdidnayem@gmail.com>
2022-11-08 22:01:44 +0600

    connect to databse

--- index.js
+++ index.js
@@ -12,12 +12,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nfm1t0q.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+async function run(){
+    try{
+        const serviceCollection = client.db("dentalSurgeon").collection("services");
+        // http://localhost:5000/services
+        app.get('/services', async(req,res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }finally{
 
+    }
+}
+run().catch(err=>console.error(err));
 
 app.get('/', (req, res) => {
     res.send("Dental Surgioun server is running");
