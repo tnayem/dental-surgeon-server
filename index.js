485f3f05bb1a640c6ec0c2fd5876f7aebf95c91f
tnayem <tasdidnayem@gmail.com>
2022-11-08 21:16:36 +0600

    basic node mongo setup

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nfm1t0q.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
     res.send("Dental Surgioun server is running");
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Dental servie is Working");
 })
