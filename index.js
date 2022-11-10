76912a9502501078cd7be98bc88281a3c9e9b360
tnayem <tasdidnayem@gmail.com>
2022-11-10 08:07:22 +0600

    add short

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,6 +15,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db("dentalSurgeon").collection("services");
+        const myServiceCollection = client.db("dentalDoctor").collection("dentalService");
         // get data from database
         // http://localhost:5000/services
         app.get('/services', async(req,res)=>{
@@ -24,11 +25,40 @@ async function run(){
             res.send(services);
         })
         // get limit data from database
-        app.get('/service', async(req,res)=>{
+        // http://localhost:5000/service
+        // app.get('/service', async(req,res)=>{
+        //     const query = {};
+        //     const cursor = serviceCollection.find(query);
+        //     const services = await cursor.limit(3).toArray();
+        //     res.send(services);
+        // })
+        //Get Limit data from database
+        
+        // add data from database
+        app.get('/doctorServices',async(req,res)=>{
             const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.limit(3).toArray();
-            res.send(services);
+            const cursor = myServiceCollection.find(query).sort({"title":1})
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        //Get Limit Data from database
+        app.get('/doctorService',async(req,res)=>{
+            const query ={}
+            const cursor = serviceCollection.find(query).sort({"title":1})
+            const result = await cursor.limit(3).toArray();
+            res.send(result)
+        })
+        // get single data from database
+        app.get('/doctorServices/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query ={_id:ObjectId(id)};
+            const result = await myServiceCollection.findOne(query)
+            res.send(result)
+        })
+        // add service from clint site
+        app.post('/doctorServices',async (req,res)=>{
+            const result = await myServiceCollection.insertOne(req.body);
+            res.send(result);
         })
     }finally{
 
